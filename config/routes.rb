db28ddaaaa8ed2f093c726775cb2a8312dcc9652
laservoid <hubertkopij@gmail.com>
2019-02-25 16:05:30 +0100

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
 
   resources :users, except: [:new]
-  
+
   post 'users', to: 'users#create'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
