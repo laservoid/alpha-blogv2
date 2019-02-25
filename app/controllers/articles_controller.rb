d4115c2b3ec9f83c75e96c80d886ba298164bafd
laservoid <hubertkopij@gmail.com>
2019-02-25 16:17:42 +0100

    Fix articles controller with user_id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = current_user
+    current_user
+    @article.user = @current_user
     if @article.save
       flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
